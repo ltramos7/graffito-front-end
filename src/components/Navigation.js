598d744f712f5b383425bb51e20ca53d534dcca7
Linda <ltramos7@gmail.com>
2020-04-13 19:46:28 -0500

    moved deleteFavorite function to App.js to try and get the Favorite.id to do the proper fetch to delete

---
+++ src/components/Navigation.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Nav from 'react-bootstrap/Nav'
+
+
+class Navigation extends Component{
+
+    handleLogout = () => {
+        localStorage.removeItem("token")
+
+    }
+
+    render(){
+        return(
+        <div>
+            <Nav >
+                <Nav.Item>
+                    <Nav.Link href="/">Home</Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                    <Nav.Link href="/murals">Murals</Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                    <Nav.Link href="/about">About</Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                    <Nav.Link href="/signup">Signup</Nav.Link>
+                </Nav.Item>
+
+                <Nav.Item>
+                    {this.props.user ?  <Nav.Link href="/" onClick={this.props.handleLogout}>
+                        Logout
+                    </Nav.Link> : <Nav.Link href="/login">
+                        Login
+                    </Nav.Link> } 
+                </Nav.Item>
+
+                <Nav.Item>
+                    {this.props.user ? <Nav.Link href= {`/profile/${this.props.user.id}`}>My Profile</Nav.Link> : null}
+                </Nav.Item>
+            </Nav>
+        </div>
+    )}
+}
+
+export default Navigation
